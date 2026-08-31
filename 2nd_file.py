3ab50004d7b788fe2825151b81ce0d9e5494a63d
Kushal Mehta <kushalmehta811@gmail.com>
2026-08-31 15:27:34 +0530

    From kushal's id

--- 2nd_file.py
+++ 2nd_file.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import ttk
 
 def greed():
+    print(f"Hello, {user_name.get() or 'World'}")
     pass
 
 root = tk.Tk() #container
@@ -15,7 +16,12 @@ name_label.pack(side="left",padx=(0,10))
 name_entry = tk.Entry(root, width=30 , textvariable=user_name)
 name_entry.pack(side="left",padx=(0,10))
 name_entry.focus()
+
+entery_button = tk.Button(root, text="Enter", command=greed)
+entery_button.pack(side="left",fill="x" , expand=True)
 # root.mainloop
 root.mainloop()
+
+
 
 
